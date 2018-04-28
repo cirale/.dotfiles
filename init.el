089ea1f5e2ee65034c2288840f04efd0f5e26d23
mol <cirale@users.noreply.github.com>
2018-04-29 02:27:01 +0900

    for 'Windows Subsystem for Linux'

--- init.el
+++ init.el
@@ -1,10 +1,18 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(global-linum-mode t)
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (mozc-popup mozc-im mozc))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,6 +26,83 @@
       (append (list nil
 		    (expand-file-name "~/.emacs.d/lib/emacs/"))
 	      load-path))
+
+;; add MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;;font
+;; デフォルト フォント
+(set-face-attribute 'default nil :family "Migu 2M" :height 120)
+;; プロポーショナル フォント
+(set-face-attribute 'variable-pitch nil :family "Migu 2M" :height 120)
+;; 等幅フォント
+(set-face-attribute 'fixed-pitch nil :family "Migu 2M" :height 120)
+;; ツールチップ表示フォント
+(set-face-attribute 'tooltip nil :family "Migu 2M" :height 90)
+
+;; Japanese Input for WSL
+(require 'mozc-im)
+(require 'mozc-popup)
+(require 'mozc-cursor-color)
+
+(setq default-input-method "japanese-mozc-im")
+
+;; popupスタイル を使用する
+(setq mozc-candidate-style 'popup)
+
+;; カーソルカラーを設定する
+(setq mozc-cursor-color-alist '((direct        . "red")
+                                (read-only     . "yellow")
+                                (hiragana      . "green")
+                                (full-katakana . "goldenrod")
+                                (half-ascii    . "dark orchid")
+                                (full-ascii    . "orchid")
+                                (half-katakana . "dark goldenrod")))
+
+;; カーソルの点滅を OFF にする
+(blink-cursor-mode 0)
+
+;; C-o で IME をトグルする
+(global-set-key (kbd "C-o") 'toggle-input-method)
+(define-key isearch-mode-map (kbd "C-o") 'isearch-toggle-input-method)
+
+;; mozc-cursor-color を利用するための対策
+(make-variable-buffer-local 'mozc-im-mode)
+(add-hook 'mozc-im-activate-hook (lambda () (setq mozc-im-mode t)))
+(add-hook 'mozc-im-deactivate-hook (lambda () (setq mozc-im-mode nil)))
+
+(advice-add 'mozc-cursor-color-update
+            :around (lambda (orig-fun &rest args)
+                      (let ((mozc-mode mozc-im-mode))
+                        (apply orig-fun args))))
+
+;; isearch を利用する前後で IME の状態を維持するための対策
+(add-hook 'isearch-mode-hook
+          (lambda ()
+            (setq im-state mozc-im-mode)))
+(add-hook 'isearch-mode-end-hook
+          (lambda ()
+            (unless (eq im-state mozc-im-mode)
+              (if im-state
+                  (activate-input-method default-input-method)
+                (deactivate-input-method)))))
+
+;; wdired 終了時に IME を OFF にする
+(require 'wdired)
+(advice-add 'wdired-finish-edit
+            :after (lambda (&rest args)
+                     (deactivate-input-method)))
+
+;; Windows の mozc では、セッション接続直後 directモード になるので hiraganaモード にする
+(advice-add 'mozc-session-execute-command
+            :after (lambda (&rest args)
+                     (when (eq (nth 0 args) 'CreateSession)
+                       ;; (mozc-session-sendkey '(hiragana)))))
+                       (mozc-session-sendkey '(Hankaku/Zenkaku)))))
+(setq mozc-helper-program-name "mozc_emacs_helper.sh")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
