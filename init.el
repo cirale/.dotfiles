d0ae4cd331b3379db5352f43a4e20c10d245dd9a
cirale <7509217+cirale@users.noreply.github.com>
2018-08-26 19:10:22 +0900

    add 00util.el

--- init.el
+++ init.el
@@ -37,7 +37,7 @@
 		    (expand-file-name "~/.emacs.d/lib/")
 		     (expand-file-name "~/.emacs.d/conf"))
 	      load-path))
-
+(load "00util")
 (load "01WSL")
 (load "02python")
 (load "03cpp")
@@ -200,30 +200,3 @@
 
 ;; F4 で tabbar-mode
 (global-set-key [f4] 'tabbar-mode)
-
-;; インストール済みパッケージのインポート/エクスポート
-(defun save-installed-packages ()
-  "Creates a file named ~/.emacs.d/emacs-packages containing all activated emacs packages."
-  (interactive)
-  (setq export-filename "~/.emacs.d/emacs-packages")
-  (if (file-exists-p export-filename) (delete-file export-filename))
-  (dolist (x package-activated-list)
-    (append-to-file
-     (format "%s\r\n" (symbol-name x))
-     nil export-filename))
-  (message "Done writing export file."))
-
-(defun restore-packages ()
-  "Installs packages listed in ~/.emacs.d/emacs-packages."
-  (interactive)
-  (setq export-filename "~/.emacs.d/emacs-packages")
-  (if (file-exists-p export-filename)
-      (mapcar
-       (lambda (package)
-         (package-install 'package))
-       (with-temp-buffer
-         (insert-file-contents export-filename)
-         (split-string (buffer-string) "\n" t))
-       )
-    (message (format "Could not find %s. Cancelling package import." export-filename)))
-  )
